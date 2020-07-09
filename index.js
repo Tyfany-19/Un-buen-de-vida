73e9a28458c33bfce22c490fc22e2c8b404f9c5e
Tyfany-19 <66805346+Tyfany-19@users.noreply.github.com>
2020-07-09 01:42:56 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".up").hide();
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1000){
+            $(".up").fadeIn("1000");
+        } else {
+            $(".up").fadeOut("1000");
+        }
+    });
+    $(".up").click(function(){
+        $('body,html').animate({
+            scrollTop:0
+        },500);
+    });
+
+});
